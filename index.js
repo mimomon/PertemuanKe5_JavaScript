623ca2138aa46d4e2a315bcfa019a52f31012c94
monicamary <poluakanmonica@gmail.com>
2023-02-27 18:00:00 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+number1 = 10;
+let number2 =20;
+const number3 = 20;
+let number = 7;
+console.log(number);
+let number2 = 2;
+let number3 = 6;
+console.log(number);
+let text = "hayow";
+console.log(text);
+const text1 = "Hello";
+const text2 = "Hello";
+console.log(number2 == 3);
+console.log(number2 > number3);
+console.log(number2 >= number3);
+console.log(number2 <= number3);
+console.log(text1 === text2);
+
+const aString = "10";
+const aNumber = "10";
+console.log(aString === aNumber);
+
+console.log(number2 > 25 && number3 < 25);
+console.log(number2 > 25 || number3 < 25);
+
+let language = "English"
+
+if (language === "English")
+{
+    console.log("English");
+} else if (language === "Bahasa Indonesia")
+{
+    console.log("Bahasa Indonesia");
+} else {
+  console.log("Deutsch");
+}  
+
+function greetings(name) {
+    console.log(`Hello ${name}`);
+}
+
+greetings("Momon");
